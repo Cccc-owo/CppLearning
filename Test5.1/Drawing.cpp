fee85c8184f5594431a385078b513074e37c8f18
Cccc_ <me@iscccc.eu.org>
2025-04-01 17:30:41 +0800

    feat: update Test5

--- Test5.1/Drawing.cpp
+++ Test5.1/Drawing.cpp
@@ -2,6 +2,97 @@
 
 using namespace std;
 
-int main() {
+typedef struct Point {
+	int x;
+	int y;
+} Point;
 
+void displayMenu();
+
+void getTwoPoints(Point* p1, Point* p2);
+
+void printPoint(Point* p);
+
+void drawCircle(Point* p1, Point* p2);
+
+void drawRectangle(Point* p1, Point* p2);
+
+int main() {
+	int choice;
+
+	Point startP, endP;
+	do {
+		displayMenu();
+		cin >> choice;
+		
+		switch (choice) {
+			case 1:	// 圆
+				getTwoPoints(&startP, &endP);
+				if (endP.x < startP.x || endP.y < startP.y) {
+					cout << "Unsupported end point!" << endl;
+					break;
+				}
+				drawCircle(&startP, &endP);
+				cout << endl;
+				break;
+			case 2: // 长方形
+				getTwoPoints(&startP, &endP);
+				if (endP.x < startP.x || endP.y < startP.y) {
+					cout << "Unsupported end point!" << endl;
+					cout << endl;
+					break;
+				}
+				drawRectangle(&startP, &endP);
+				cout << endl;
+				break;
+			case 0:
+				cout << "Good bye!" << endl;
+				break;
+			default:
+				cout << "Not supported! Please select again!" << endl;
+				cout << endl;
+				break;
+		}
+	} while (choice);
+
+	return 0;
+}
+
+void displayMenu() {
+	cout << "******************************" << endl;
+	cout << "    1. Circle (圆)            " << endl;
+	cout << "    2. Rectangle (长方形)      " << endl;
+	cout << "    0. Exit (退出)            " << endl;
+	cout << "******************************" << endl;
+	cout << "Please select the shape: ";
+}
+
+void getTwoPoints(Point* p1, Point* p2) {
+	cout << "Please input the coordinate (x, y) of the start point: ";
+	cin >> p1->x >> p1->y;
+	cout << "Please input the coordinate (x, y) of the end point: ";
+	cin >> p2->x >> p2->y;
+}
+
+void printPoint(Point* p) {
+	cout << "a point at (" << p->x << ", " << p->y << ")";
+}
+
+void drawCircle(Point* p1, Point* p2) {
+	Point center;
+	double radius = 0;
+	if (p2->x - p1->x != p2->y - p1->y) {
+		cout << "Not a circle, select again." << endl;
+	} else {
+		radius = (p2->x - p1->x) / 2.0;
+		center.x = (p1->x + p2->x) / 2;
+		center.y = (p1->y + p2->y) / 2;
+		cout << "Draw a circle at center (" << center.x << ", " << center.y << ") with radius " << radius << endl;
+	}
+}
+
+void drawRectangle(Point* p1, Point* p2) {
+	int width = p2->x - p1->x;
+	int height = p2->y - p1->y;
+	cout << "Draw a rectangle at topleft (" << p1->x << ", " << p1->y << "), whose width is " << width << " and height is " << height << "." << endl;
 }
--- Test5.2/Taxes.cpp
+++ Test5.2/Taxes.cpp
@@ -1,7 +1,79 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main() {
+typedef struct {
+	int base;
+	int rate;
+} Rule;
 
+void inputRules(Rule rules[], int n);
+
+void printRules(Rule rules[], int n);
+
+double computeTax(Rule rules[], int n, int income);
+
+int main() {
+	Rule rules[100];
+	int amount = 0;
+	int income = -1;
+	double taxes = 0.0;
+
+	cout << "请输入规则的条数：";
+	cin >> amount;
+	if (amount <= 0) {
+		cout << "输入数值不合法！" << endl;
+		return 0;
+	}
+
+	inputRules(rules, amount);
+	cout << endl;
+
+	printRules(rules, amount);
+	cout << endl;
+
+	do {
+		cout << "请输入您的收入：";
+		cin >> income;
+		if (income == -1) {
+			cout << "再见！" << endl;
+			break;
+		}
+		taxes = computeTax(rules, amount, income);
+		cout << "您的收入是：" << income << "，应缴所得税：" << fixed << setprecision(2) << taxes << " 元。" << endl;
+		cout << endl;
+	} while (income != -1);
+
+	return 0;
+}
+
+void inputRules(Rule rules[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "请输入第" << i + 1 << "条规则：";
+		cin >> rules[i].base >> rules[i].rate;
+	}
+}
+
+void printRules(Rule rules[], int n) {
+	cout << "纳税规则如下：" << endl;
+	cout << "纳税线      税率" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << setw(8) << left << rules[i].base << setw(6) << right << rules[i].rate << endl;
+	}
+}
+
+double computeTax(Rule rules[], int n, int income) {
+	double taxes = 0.0;
+	for (int i = n - 1; i >= 0; i--) {
+		if (income <= rules[i].base) {
+			continue;
+		}
+		int difference = income - rules[i].base;
+		// cout << endl << difference << endl;
+		taxes += difference * rules[i].rate / 100.0;
+		income -= difference;
+		// cout << endl << taxes << endl;
+	}
+	return taxes;
 }
